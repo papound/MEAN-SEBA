dcdebdf612447efe6b6eb969a486863486ecf407
Jatupat Chaiprasert <jatupat.chaiprasert@tum.de>
2016-06-02 00:16:17 +0200

    - Comple DateTime Picker (Format: DDMMYYYY) - Input box/ Checkbox/button still not  working-> Want to add AngularJS - Still trying them on test-Prize.html

--- public/newTemplate/js/Angular_Button.js
+++ public/newTemplate/js/Angular_Button.js
@@ -2,3 +2,10 @@
  * Created by Jatupat Chaiprasert on 01-Jun-16.
  */
 
+angular.module('buttonsDemo1', ['ngMaterial'])
+.controller('AppCtrl', function($scope) {
+  $scope.title1 = 'Button';
+  $scope.title4 = 'Warn';
+  $scope.isDisabled = true;
+  $scope.googleUrl = 'http://google.com';
+});
---
+++ public/newTemplate/js/Angular_CheckBox.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Jatupat Chaiprasert on 01-Jun-16.
+ */
+angular.module('checkboxDemo1', ['ngMaterial'])
+    .controller('AppCtrl', function ($scope) {
+        $scope.data = {};
+        $scope.data.cb1 = true;
+        $scope.data.cb2 = false;
+        $scope.data.cb3 = false;
+        $scope.data.cb4 = false;
+        $scope.data.cb5 = false;
+    });
